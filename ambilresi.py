ca5c225e5f5968a2e45a257d944aa2f7cc475c38
niddumulu <36333121+niddumulu@users.noreply.github.com>
2021-01-17 22:47:50 +0700

    Create ambilresi.py
    @cekresijnt

---
+++ ambilresi.py
@@ -0,0 +1,25 @@
+import os
+import time
+from telethon import TelegramClient, sync, events, utils
+api_id = 682610 
+api_hash = '030132b51d598e464419ccee7f20212d' 
+client = TelegramClient('mbanurul', api_id, api_hash).start()
+seconds= int(0)
+minutes= int(0)
+run = "siap"
+while run.lower()=="siap":
+  if seconds == 59:
+      seconds = 0
+      minutes = minutes+1
+      if minutes == 1:
+        print ("ok")
+        for message in client.iter_messages('cekresii', limit=1):
+          (utils.get_display_name(message.sender), message.message)
+          pesan = (message.message)
+          print (pesan)
+        client.send_message('cekresijnt', pesan)
+      if minutes == 3:
+        minutes = 0  
+  seconds = (seconds+1)
+  print (minutes,seconds)
+  time.sleep(1)
