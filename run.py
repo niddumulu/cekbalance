7ffd3ced62f71f86bc67eff011d3bb6614b88b44
niddumulu <36333121+niddumulu@users.noreply.github.com>
2021-01-18 12:55:04 +0700

    Update run.py

--- run.py
+++ run.py
@@ -6,11 +6,6 @@ api_id = 682610
 api_hash = '030132b51d598e464419ccee7f20212d' 
 client = TelegramClient('mbanurul', api_id, api_hash).start()
 
-#for message in client.iter_messages('cekresii', limit=1):
- #         (utils.get_display_name(message.sender), message.message)
-  #        pesan = (message.message)
-   #       print (pesan)
-
 seconds = int(0)
 minutes = int(0)
 
@@ -18,15 +13,14 @@ run = "dia"
 while run.lower()=="dia":
   if seconds == 10:
     seconds = 0
-    minutes = minutes+1
-    #if minutes == 1:
-      #print ("sahal")
+    print ("cek tanda")
+    for message in client.iter_messages('pohonide', limit=1):
+        (utils.get_display_name(message.sender), message.message)
+         pesan = (message.message)
+         print (pesan)
       #os.system("python robotgabung.py")
-    #if minutes == 5:
-      #print ("sahil")
-      #minutes = 0
-      
-  os.system('clear')
+    minutes = minutes+1   
+  #os.system('clear')
   seconds = (seconds+1)
-  #print (minutes," : ", seconds)
+  print (minutes," : ", seconds)
   time.sleep(1)
